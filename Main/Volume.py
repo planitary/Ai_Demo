3187b12a3416377f3a40bc3841ec74958b72fecf
planitary <853652156@qq.com>
2021-07-20 22:57:56 +0800

    Modify

--- Main/Volume.py
+++ Main/Volume.py
@@ -36,6 +36,24 @@ class Cal_Voume(BaseErrorInfo):
             Volume = math.pow(length,3)
             print("正方体的体积是:%.2f" % Volume)
 
+    # 圆柱体
+    def Radius(self,radius,height):
+        try:
+            if radius < 0 or height < 0:
+                raise PositiveIntegerError
+        except Exception as e:
+            if radius < 0:
+                print(e)
+                logging.error("-----****半径%d不合法***-----" % radius, exc_info=True, stack_info=True)
+            if height < 0:
+                print(e)
+                logging.error("-----****高度%d不合法***-----" % height, exc_info=True, stack_info=True)
+        else:
+            S = math.pi * radius * radius
+            Area = S * height
+            print("圆柱体的体积为：%.3f" % Area)
+
 if __name__ == '__main__':
     newShape = Cal_Voume()
-    newShape.Cuboid(-12,-3,-4)
+    newShape.Radius(3,7)
+    newShape.Radius(-3,-5)
