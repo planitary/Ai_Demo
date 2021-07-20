e878c0eb6a98a462fcc29a8e2fbc50708aea6e1b
planitary <853652156@qq.com>
2021-07-20 11:18:18 +0800

    V1.0-Modify

--- Log/LogConfig.py
+++ Log/LogConfig.py
@@ -7,7 +7,9 @@ LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 # 格式化日期
 DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 str = time.strftime("%Y%m%d-%H-%M-%S",time.localtime())
+
+'''不同系统下的日志路径，非绝对，需根据实际情况进行变更'''
 # windows日志路径
-# logging.basicConfig(level=logging.DEBUG,format=LOG_FORMAT,datefmt=DATE_FORMAT,filename='Log\%s.log' % str)
+logging.basicConfig(level=logging.DEBUG,format=LOG_FORMAT,datefmt=DATE_FORMAT,filename='..\Log\%s.log' % str)
 # mac日志路径
-logging.basicConfig(level=logging.DEBUG,format=LOG_FORMAT,datefmt=DATE_FORMAT,filename='../Log/%s.log' % str)
+# logging.basicConfig(level=logging.DEBUG,format=LOG_FORMAT,datefmt=DATE_FORMAT,filename='../Log/%s.log' % str)
